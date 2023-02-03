5421ede4e68a59c551ab510520b7253e3e04440e
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2023-02-03 20:19:01 +0530

    3rd FEB class

---
+++ src/classNotes/Feb3.java
@@ -0,0 +1,48 @@
+package classNotes;
+
+//CODE 1
+class Maruthi{
+	int airbags = 2; 
+	double mileage ; 
+}
+
+public class Feb3 {
+
+	public static void main(String[] args) {
+		// keywords are lower case
+		// class members -> variable(state) , methods(behavior) , constructors(initial state of var)
+		// 8 primitive type
+		// access specifiers -> private , public , protected , package private (name workspace of related classes)
+		// public static int x ; => static means class variable
+		// Constructor : className(params) , no return type , access type is same as class
+		// Method : same rules are main
+		
+		// OBJECTS
+		// className objectName ; => does not define only refers (objectName contains NULL)
+		// new  : dynamically allocate at run time. 
+		// objectName  = new className(); -> envoke a constructor
+		// className objectName = new className(); 
+		// Default constructor exist 
+		// (.) after object to access variables and methods
+		
+		
+		// CODE 1
+//		Maruthi ciaz = new Maruthi(); 
+//		ciaz.airbags = 3 ;
+//		ciaz.mileage = 12.5; 
+//		System.out.println(ciaz.airbags);
+//		System.out.println(ciaz.mileage);
+		 
+		// CODE 2
+//		Maruthi ciaz = new Maruthi(); 
+//		ciaz.airbags = 3;
+//		ciaz.mileage = 12.5; 
+//		Maruthi b  = new Maruthi(); 
+//		b.airbags = 2; 
+//		b.mileage = 13.8; 
+//		System.out.println("Ciaz->" + " " + ciaz.airbags +  " " + ciaz.mileage);
+//		System.out.println("B->" + " " + b.airbags +  " " + b.mileage);
+//		
+	}
+
+}
