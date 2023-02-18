849413835d332d3c0a50742bdc2776c08383c2a1
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2023-02-18 12:41:47 +0530

    17th Feb COMMAND LINE AND VARIABLE LENGTH ARGUMENTS

--- src/classNotes/Feb15_17.java
+++ src/classNotes/Feb15_17.java
@@ -21,12 +21,30 @@ class Class2{
 	int age ;
 }
 
+class Class3{
+	int sum(int ...a) {
+		int temp = 0; 
+		int n = a.length; 
+		for(int i = 0 ; i < n ; i++) {
+			temp += a[i]; 
+		}
+		return temp; 
+	}
+}
+
 
 public class Feb15_17 {
 
 	public static void main(String[] args) {
 		
+		
+		
 		// CLONE CLASS
+		
+		
+		
+		
+		
 		// Assignment operator will create a copy of variables and not of objects.
 		// Method to copy objects
 		//		1-> By using constructor
@@ -35,17 +53,6 @@ public class Feb15_17 {
 		// Classname name1 = name2; => creates an reference to name2 class which means they point to the same address. any changes in name1 are also refelected in name2
 		// Every class that implements clone() should call super.clone() to obtain the cloned object reference.
 	
-		
-		
-		
-		// FINAL KEYWORD
-		// Used to create constant variables , Used to prevent method overriding , Used to prevent extending classes; 
-		// When a variable is declared with final keyword, it’s value cannot be modified => const
-		// The final variable has to be initialized otherwise the it java will throw a compile-time error.
-		// A final variable cannot be re-assigned, but in case of a reference final variable, internal state of the object pointed by that reference variable can be changed.
-		// A final variable cannot be reassigned, doing it, will throw compile-time error.
-		// Blank initialization of final variables is allowed
-		
 		
 		
 		// Cloning a class
@@ -57,6 +64,25 @@ public class Feb15_17 {
 //		System.out.println(c2.name + " " + c2.age); 
 //		c1.clone(c2);
 //		System.out.println(c2.name + " " + c2.age); 
+		
+		
+		
+		
+		
+		
+		// FINAL KEYWORD
+		
+		
+		
+		
+		
+		// Used to create constant variables , Used to prevent method overriding , Used to prevent extending classes; 
+		// When a variable is declared with final keyword, it’s value cannot be modified => const
+		// The final variable has to be initialized otherwise the it java will throw a compile-time error.
+		// A final variable cannot be re-assigned, but in case of a reference final variable, internal state of the object pointed by that reference variable can be changed.
+		// A final variable cannot be reassigned, doing it, will throw compile-time error.
+		// Blank initialization of final variables is allowed
+		
 		
 		
 		// Final keyword
@@ -73,6 +99,31 @@ public class Feb15_17 {
 		// final class can not be inherited
 		// creates an immutable class like the predefined String class.
 		
+		
+		
+		
+		
+		
+		// COMMAND LINE AND VARIABLE LENGTH ARGUMENTS
+		
+		
+		
+		
+		
+		//  java class_name command_line_arguments
+		//  JVM wraps the command-line arguments and supplies to args [] => args.length to get the length
+		// A method that takes a variable number of arguments is a varargs method
+		// (int ...a) => 3 dots => method can be called with zero or more args
+		// var x is implicitly declared as an array of type int[]
+		// only one varargs parameter can exit in a method => always at the end. 
+		// Var args methods can be overloaded just like normal methods. 
+		
+		Class3 x = new Class3(); 
+		int ans = x.sum(1,2,3,4,5,6); 
+		System.out.println(ans);
+		
+		
+
 	
 		
 		
