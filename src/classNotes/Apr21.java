e02be4e070088a3556a6b0315f780065b84468e7
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2023-04-21 22:02:03 +0530

    Threads Apr21 Night class

--- src/classNotes/Apr21.java
+++ src/classNotes/Apr21.java
@@ -2,23 +2,106 @@ package classNotes;
 import static java.lang.System.*; 
 
 
-class thread implements Runnable{
+
+// Code 2
+//class thread implements Runnable{
+//	public void run() {
+//		out.print("In the run thread");
+//		try {
+//			for(int i = 0 ; i < 100 ; i++) {
+//				out.println("hello from run"); 
+//				Thread.sleep(1000);
+//			}
+//		}catch(InterruptedException e) {
+//			out.println("handle expection in child"); 
+//		}
+//		out.println("Exiting the thread"); 
+//		}
+//	
+//		public void display() {
+//			out.println("display in run");
+//		}
+//}
+
+
+
+// 	Code 3
+//class thread2 implements Runnable{
+//	String name;  
+//	thread2(String s1){
+//		this.name = s1; 
+//		Thread t = new Thread(this , name); 
+//		System.out.println("New thread : " + t); 
+//		t.start(); 
+//	}
+//	
+//	public void run() {
+//		try {
+//			for(int i = 0 ; i < 10 ; i++) {
+//				out.println(name + " " + i); 
+//				Thread.sleep(1000); 
+//			}
+//		}catch(InterruptedException e) {
+//			out.println("Exiting"); 
+//		}	
+//	}
+//}
+
+
+
+// Code 4
+//class thread4 implements Runnable{
+//	thread4(){
+//		Thread t = new Thread(this); 
+//		t.start(); 
+//	}
+//	
+//	public void run() {
+//		try {
+//			for(int i = 0 ; i < 10 ; i++) {
+//				out.println(i); 
+//				Thread.sleep(1000); 
+//			}
+//		}catch(InterruptedException e) {
+//			out.println("Exiting"); 
+//		}	
+//	}
+//}
+
+
+
+// Code 5
+// Can not extend other classes because that will lead to multiple inheritance
+//class thread5 extends Thread{
+//	public void run() {
+//		try {
+//			for(int i = 0 ; i < 10 ; i++) {
+//				out.println(i); 
+//				Thread.sleep(1000); 
+//			}
+//		}catch(InterruptedException e) {
+//			out.println("Exiting"); 
+//		}	
+//	}
+//}
+
+
+// Code 6
+class thread6 implements Runnable{
 	public void run() {
-		out.print("In the run thread");
 		try {
-			for(int i = 0 ; i < 100 ; i++) {
-				out.println("hello from run"); 
-				Thread.sleep(1000);
+			for(int i = 0 ; i < 10 ; i++) {
+				out.println(i); 
+				Thread.sleep(1000); 
 			}
 		}catch(InterruptedException e) {
-			out.println("handle expection in child"); 
+			out.println("Exiting"); 
 		}
-		out.println("Exiting the thread"); 
-		}
-	
-		public void display() {
-			out.println("display in run");
-		}
+		out.println("Exited"); 
+	}
+	public void display() {
+		out.println("display function"); 
+	}
 }
 
 public class Apr21 {
@@ -82,22 +165,104 @@ public class Apr21 {
 		
 		
 		// Code 2
-		thread t1 = new thread(); 
-		Thread t = new Thread(t1); 
-		t.start();
-		out.println("Thread Called : " + t); 
-		t1.display();
+//		thread t1 = new thread(); 
+//		Thread t = new Thread(t1); 
+//		t.start();
+//		out.println("Thread Called : " + t); 
+//		t1.display();
+//		
+//		try {
+//			for(int i = 100 ; i >= 0 ; i--) {
+//				out.println("hello from main"); 
+//				Thread.sleep(2000);
+//			}
+//		}catch(InterruptedException e) {
+//			out.println("handle expection in child"); 
+//		}
+//		out.println("Exiting the main thread"); 
+	
 		
+		
+		// Code 3
+//		new thread2("One"); 
+//		new thread2("Two"); 
+//		new thread2("Three"); 
+//		try {
+//			Thread.sleep(2000);
+//		}catch(InterruptedException e) {
+//			out.println("Main ending"); 
+//		}
+//		out.println("Code 3 over"); 
+		
+		
+		
+		// Code 4
+//		thread4 t = new thread4(); 
+//		try {
+//			Thread.sleep(10000);
+//		}catch(InterruptedException e) {
+//			out.println("Exiting main"); 
+//		}
+//		out.println("end of code 4"); 
+		
+		
+		// Code 5
+//		thread5 t = new thread5(); 
+//		t.start();
+//		try {
+//			Thread.sleep(5000);
+//		}catch(InterruptedException e) {
+//			out.println("Exiting main"); 
+//		}
+//		out.println("end of code 5"); 
+		
+		
+		
+		
+		
+		// Life Cycle of Threads
+		// Born Thread -> new thread just created
+		// Runnable
+		// Running
+		// Waiting -> The threads needs signal for execution needs to resume
+		// Dead -> finished execution / Can enter dead directly from the waiting
+		// Suspended -> Halt execution but can be resume from where it was suspended. 
+		// Terminated -> Halt execution but can not be resumed.
+		
+		
+		
+		
+		
+		// Code 6
+		thread6 t1 = new thread6(); 
+		Thread t = new Thread(t1); 
+		thread6 t2 = new thread6();
+		Thread tx = new Thread(t2); 
+		t.start(); 
+		tx.start();
+		out.println(t); 
+		//tx.interrupt();
+		tx.stop(); 
 		try {
-			for(int i = 100 ; i >= 0 ; i--) {
-				out.println("hello from main"); 
-				Thread.sleep(2000);
+			for(int i = 1000 ; i >= 990 ; i--) {
+				out.println(i); 
+				Thread.sleep(1000);
 			}
 		}catch(InterruptedException e) {
-			out.println("handle expection in child"); 
+			out.println("Exiting main"); 
 		}
-		out.println("Exiting the main thread"); 
-	
+		out.println("end of code 6"); 
+		tx.resume(); 
+		out.println("End"); 
+		
+		
+		
+		
+		
+		
+		
 	}
-
 }
+		
+		
+		
